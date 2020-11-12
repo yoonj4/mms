6d1e2fd72227ffa3d42f3de9ccd72876248d0478
yoonj4 <justin.o.yoon@gmail.com>
2020-11-12 14:40:06 -0800

    Implement sendVideo. Add pedantic lint rules.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,8 @@
+# This project uses Semantic Versioning
+## 0.1.0
+
+* Use platform interface.
+
 ## 0.0.1
 
-* TODO: Describe initial release.
+* Initial commit.
--- lib/mms.dart
+++ lib/mms.dart
@@ -2,6 +2,7 @@
 import 'dart:async';
 
 import 'package:flutter/services.dart';
+import 'package:mms_platform_interface/mms_platform_interface.dart';
 
 class Mms {
   static const MethodChannel _channel =
@@ -11,12 +12,16 @@ class Mms {
     final String version = await _channel.invokeMethod('getPlatformVersion');
     return version;
   }
+
+  static Future<bool> sendVideo(
+      final String videoFilePath,
+      final List<String> recipientNumbers) {
+    assert(videoFilePath != null);
+    assert(recipientNumbers != null);
+    assert(recipientNumbers.isNotEmpty);
+    return MmsPlatform.instance.sendVideo(videoFilePath, recipientNumbers);
+  }
 }
-// TODO: Look at static method vs callable class:
 // example before step 3: https://medium.com/flutter/how-to-write-a-flutter-web-plugin-part-2-afdddb69ece6
 // https://developer.android.com/reference/android/telephony/SmsManager?fbclid=IwAR32iB0aWcgdmfAiD0MXGvitqM2TtzKayFXkPjxi6DaCIehJgjw3Y6QZz7g#sendMultimediaMessage(android.content.Context,%20android.net.Uri,%20java.lang.String,%20android.os.Bundle,%20android.app.PendingIntent)
 // https://forums.xamarin.com/discussion/181087/xamarin-android-smsmanager-sendmultimediamessage-mms?fbclid=IwAR1vQxAxIV-hjxASjpsYOeE55tjpfU5zAvtAGhhXqjh8NluRyylVRHldgnQ
-// https://dart.dev/guides
-// https://dart.dev/samples#classes
-// https://dart.dev/codelabs/dart-cheatsheet
-// https://dart.dev/guides/language/language-tour
