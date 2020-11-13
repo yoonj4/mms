9890099551a19f4c1bd14990ec0063c770645eee
yoonj4 <justin.o.yoon@gmail.com>
2020-11-12 20:30:07 -0800

    Make sendVideo an instance method. Finish example app.

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,10 +1,19 @@
+import 'dart:io';
+
+import 'package:camera/camera.dart';
+import 'package:contacts_service/contacts_service.dart';
 import 'package:flutter/material.dart';
 import 'dart:async';
 
-import 'package:flutter/services.dart';
 import 'package:mms/mms.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:permission_handler/permission_handler.dart';
 
-void main() {
+List<CameraDescription> cameras;
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  cameras = await availableCameras();
   runApp(MyApp());
 }
 
@@ -14,45 +23,147 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  String _platformVersion = 'Unknown';
+  CameraController _controller;
+  String _videoPath;
+
+  final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
 
   @override
   void initState() {
     super.initState();
-    initPlatformState();
+
+    _controller = CameraController(cameras[0], ResolutionPreset.medium);
+    _controller.initialize().then((value) {
+      if (!mounted) {
+        return;
+      }
+      setState(() {
+
+      });
+    });
   }
 
-  // Platform messages are asynchronous, so we initialize in an async method.
-  Future<void> initPlatformState() async {
-    String platformVersion;
-    // Platform messages may fail, so we use a try/catch PlatformException.
-    try {
-      platformVersion = await Mms.platformVersion;
-    } on PlatformException {
-      platformVersion = 'Failed to get platform version.';
-    }
-
-    // If the widget was removed from the tree while the asynchronous platform
-    // message was in flight, we want to discard the reply rather than calling
-    // setState to update our non-existent appearance.
-    if (!mounted) return;
-
-    setState(() {
-      _platformVersion = platformVersion;
-    });
+  @override
+  void dispose() {
+    _controller?.dispose();
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+        key: _scaffoldKey,
         appBar: AppBar(
           title: const Text('Plugin example app'),
         ),
-        body: Center(
-          child: Text('Running on: $_platformVersion\n'),
+        body: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          mainAxisSize: MainAxisSize.max,
+          children: <Widget>[
+            IconButton(
+              icon: const Icon(Icons.videocam),
+              color: Colors.blue,
+              onPressed: _controller != null &&
+                  _controller.value.isInitialized &&
+                  !_controller.value.isRecordingVideo
+                  ? onVideoRecordButtonPressed
+                  : null,
+            ),
+            IconButton(
+              icon: const Icon(Icons.stop),
+              color: Colors.red,
+              onPressed: _controller != null &&
+                  _controller.value.isInitialized &&
+                  _controller.value.isRecordingVideo
+                  ? onStopButtonPressed
+                  : null,
+            )
+          ],
         ),
       ),
     );
+  }
+
+  void showInSnackBar(String message) {
+    // ignore: deprecated_member_use
+    _scaffoldKey.currentState.showSnackBar(SnackBar(content: Text(message)));
+  }
+
+  String timestamp() => DateTime.now().millisecondsSinceEpoch.toString();
+
+  void logError(String code, String message) =>
+      print('Error: $code\nError Message: $message');
+
+  void onVideoRecordButtonPressed() {
+    startVideoRecording().then((String filePath) {
+      if (mounted) setState(() {});
+      if (filePath != null) showInSnackBar('Saving video to $filePath');
+    });
+  }
+
+  void onStopButtonPressed() {
+    stopVideoRecording().then((_) {
+      if (mounted) setState(() {});
+      showInSnackBar('Video recorded to: $_videoPath');
+      Permission.contacts.request().isGranted.then((value) {
+        if (value) {
+          ContactsService.getContacts()
+              .then((value) {
+                  if (value.isNotEmpty) {
+                    Mms().sendVideo(_videoPath,
+                        value.map((e) => e.phones.first.value).toList());
+                  } else {
+                    showInSnackBar('Please add contacts.');
+                  }
+              }
+          );
+        }
+      });
+    });
+  }
+
+  Future<String> startVideoRecording() async {
+    if (!_controller.value.isInitialized) {
+      showInSnackBar('Error: select a camera first.');
+      return null;
+    }
+
+    final Directory extDir = await getApplicationDocumentsDirectory();
+    final String dirPath = '${extDir.path}/Movies/flutter_test';
+    await Directory(dirPath).create(recursive: true);
+    final String filePath = '$dirPath/${timestamp()}.mp4';
+
+    if (_controller.value.isRecordingVideo) {
+      // A recording is already started, do nothing.
+      return null;
+    }
+
+    try {
+      _videoPath = filePath;
+      await _controller.startVideoRecording(filePath);
+    } on CameraException catch (e) {
+      _showCameraException(e);
+      return null;
+    }
+    return filePath;
+  }
+
+  Future<void> stopVideoRecording() async {
+    if (!_controller.value.isRecordingVideo) {
+      return null;
+    }
+
+    try {
+      await _controller.stopVideoRecording();
+    } on CameraException catch (e) {
+      _showCameraException(e);
+      return null;
+    }
+  }
+
+  void _showCameraException(CameraException e) {
+    logError(e.code, e.description);
+    showInSnackBar('Error: ${e.code}\n${e.description}');
   }
 }
--- lib/mms.dart
+++ lib/mms.dart
@@ -13,7 +13,7 @@ class Mms {
     return version;
   }
 
-  static Future<bool> sendVideo(
+  Future<bool> sendVideo(
       final String videoFilePath,
       final List<String> recipientNumbers) {
     assert(videoFilePath != null);
@@ -22,6 +22,5 @@ class Mms {
     return MmsPlatform.instance.sendVideo(videoFilePath, recipientNumbers);
   }
 }
-// example before step 3: https://medium.com/flutter/how-to-write-a-flutter-web-plugin-part-2-afdddb69ece6
 // https://developer.android.com/reference/android/telephony/SmsManager?fbclid=IwAR32iB0aWcgdmfAiD0MXGvitqM2TtzKayFXkPjxi6DaCIehJgjw3Y6QZz7g#sendMultimediaMessage(android.content.Context,%20android.net.Uri,%20java.lang.String,%20android.os.Bundle,%20android.app.PendingIntent)
 // https://forums.xamarin.com/discussion/181087/xamarin-android-smsmanager-sendmultimediamessage-mms?fbclid=IwAR1vQxAxIV-hjxASjpsYOeE55tjpfU5zAvtAGhhXqjh8NluRyylVRHldgnQ
