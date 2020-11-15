cf27178b2f5c9fb3557fc483102e22f9a5027a4f
yoon <justin.o.yoon@gmail.com>
2020-11-15 01:33:25 -0800

    Use fromFile() for creating URI.

--- android/src/main/kotlin/com/yoonj4/mms/Mms.kt
+++ android/src/main/kotlin/com/yoonj4/mms/Mms.kt
@@ -4,26 +4,23 @@ import android.content.Context
 import android.content.Intent
 import android.content.Intent.FLAG_ACTIVITY_NEW_TASK
 import android.net.Uri
-import android.os.Build
-import android.os.StrictMode
 import androidx.annotation.NonNull
 import androidx.core.content.ContextCompat.startActivity
-import java.lang.reflect.Method
-
+import java.io.File
 
 class Mms(@NonNull private val context: Context) {
 
     fun sendVideo(@NonNull videoFilePath: String, @NonNull recipientNumbers: List<String>) {
 
-        if (Build.VERSION.SDK_INT >= 24) {
-            try {
-                val m: Method = StrictMode::class.java.getMethod("disableDeathOnFileUriExposure")
-                m.invoke(null)
-            } catch (e: Exception) {
-                e.printStackTrace()
-            }
-        }
-        val contentUri = Uri.parse("file://$videoFilePath")
+//        if (Build.VERSION.SDK_INT >= 24) {
+//            try {
+//                val m: Method = StrictMode::class.java.getMethod("disableDeathOnFileUriExposure")
+//                m.invoke(null)
+//            } catch (e: Exception) {
+//                e.printStackTrace()
+//            }
+//        }
+        val contentUri = Uri.fromFile(File(videoFilePath))
         recipientNumbers.forEach {
             val sendIntent = Intent(Intent.ACTION_SEND)
             sendIntent.putExtra("address", it)
