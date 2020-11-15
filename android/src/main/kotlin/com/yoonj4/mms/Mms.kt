f26dd110ac1068c934a409b9e41fd6a3eea77643
yoon <justin.o.yoon@gmail.com>
2020-11-15 01:35:37 -0800

    Add hack back.

--- android/src/main/kotlin/com/yoonj4/mms/Mms.kt
+++ android/src/main/kotlin/com/yoonj4/mms/Mms.kt
@@ -4,22 +4,25 @@ import android.content.Context
 import android.content.Intent
 import android.content.Intent.FLAG_ACTIVITY_NEW_TASK
 import android.net.Uri
+import android.os.Build
+import android.os.StrictMode
 import androidx.annotation.NonNull
 import androidx.core.content.ContextCompat.startActivity
 import java.io.File
+import java.lang.reflect.Method
 
 class Mms(@NonNull private val context: Context) {
 
     fun sendVideo(@NonNull videoFilePath: String, @NonNull recipientNumbers: List<String>) {
 
-//        if (Build.VERSION.SDK_INT >= 24) {
-//            try {
-//                val m: Method = StrictMode::class.java.getMethod("disableDeathOnFileUriExposure")
-//                m.invoke(null)
-//            } catch (e: Exception) {
-//                e.printStackTrace()
-//            }
-//        }
+        if (Build.VERSION.SDK_INT >= 24) {
+            try {
+                val m: Method = StrictMode::class.java.getMethod("disableDeathOnFileUriExposure")
+                m.invoke(null)
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+        }
         val contentUri = Uri.fromFile(File(videoFilePath))
         recipientNumbers.forEach {
             val sendIntent = Intent(Intent.ACTION_SEND)
